ae47250e7b3695c22b6482bff3a66283d88efa48
chayut kliewklom <chayukli@scg.com>
2022-06-27 21:25:17 +0700

    MergeCode

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
+      <a href="https://www.scgpackaging.com/" target="_blank">SCGP</a>
+      <span class="ml-1">&copy; {{new Date().getFullYear()}} SCGP - IT</span>
     </div>
     <div class="mfs-auto">
       <span class="mr-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <a href="https://visyproductmaster.scg.com">SCGP - IT</a>
     </div>
   </CFooter>
 </template>
